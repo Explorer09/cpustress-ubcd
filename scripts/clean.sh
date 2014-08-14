fc761b08fe56d2d19b26b603d714fb5baca3765a
Explorer09 <explorer09@gmail.com>
2014-08-15 01:06:06 +0800

    Update documentation and scripts for CPUstress v2.5.0.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,9 +7,9 @@
 cd `dirname $0`
 cd ..
 
-ISO_FILENAME="cpustress-develop.iso"
+ISO_FILENAME="cpustress-2.5.0.iso"
 
-ARCHIVE_NAME="cpustress-develop"
+ARCHIVE_NAME="cpustress-2.5.0"
 
 rm -f "$ISO_FILENAME"
 rm -f "${ARCHIVE_NAME}.7z"
