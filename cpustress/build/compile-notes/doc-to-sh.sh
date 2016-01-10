52901c2c4b14f06c9462bba021c0e8ab3bdd3e15
Explorer09 <explorer09@gmail.com>
2016-01-10 20:15:11 +0800

    New doc-to-sh.sh .
    This also process flex-bison.txt and has an ability to embed the content of
    option-groups.config into the generated toolchain.sh .

--- cpustress/build/compile-notes/doc-to-sh.sh
+++ cpustress/build/compile-notes/doc-to-sh.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-awk '
-BEGIN {
-    print "#!/bin/bash";
-}
-{
-    if ($0 ~ /^ \$ /) {
-        is_code = 1;
-        sub(/^ \$ /, "");
-        print ;
-    } else if (is_code == 1 && $0 ~ /^     /) {
-        sub(/^     /, "");
-        print ;
-    } else {
-        is_code = 0;
-        if ($0 == "")
-            print "";
+for file in toolchain flex-bison; do
+    echo "#!/bin/bash" >${file}.sh
+    if [ "$file" = toolchain ]; then
+        echo "write_option_groups_config () {"
+        if [ -f eglibc/option-groups.config ]; then
+            echo "cat > option-groups.config << EOF"
+            cat eglibc/option-groups.config
+            echo "EOF"
         else
-            print "# " $0;
+            # Function must not be empty.
+            echo ":"
+        fi
+        echo "}"
+    fi >>${file}.sh
+    awk '
+    {
+        if ($0 == " $ [ -f option-groups.config ] || exit 1") {
+            print "write_option_groups_config";
+        }
+        if ($0 ~ /^ \$ /) {
+            is_code = 1;
+            sub(/^ \$ /, "");
+            print ;
+        } else if (is_code == 1 && $0 ~ /^     /) {
+            sub(/^     /, "  ");
+            print ;
+        } else {
+            is_code = 0;
+            if ($0 == "")
+                print "";
+            else
+                print "# " $0;
+        }
     }
-}
-' toolchain.txt >toolchain.sh
+    ' ${file}.txt >>${file}.sh
+done
